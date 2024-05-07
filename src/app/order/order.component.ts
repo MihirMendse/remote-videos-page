091981f0949a92a9f1c30fd0f43b1d7cc6f20160
MihirMendse <mihiram515@gmail.com>
2024-05-07 12:24:39 +0530

    Initial commit

---
+++ src/app/order/order.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent {
+constructor(private route: Router){}
+cartNavigate(){
+  this.route.navigate(['/cart'])
+}
+
+}
